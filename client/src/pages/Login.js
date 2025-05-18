0eaddfc14e34bbd4d08b044fc37ad024f2e711b7
pacyuzu16 <133155388+pacyuzu16@users.noreply.github.com>
2025-05-18 15:57:32 +0200

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,7 +15,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const response = await axios.post('http://citizen-engagement-backend.onrender.com/api/auth/login', formData);
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('role', response.data.role);
       setError('');
